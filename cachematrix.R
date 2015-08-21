b4a1c2855f5aff516cbe7d8fd2f23b006850fcd9
Max <wheelerrm92@gmail.com>
2015-08-21 12:21:20 -0400

    Fleshed out the program to hopefully its final form

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+#These functions create a matrix class that can store both the starting matrix and its inverse
 
-## Write a short comment describing this function
+##makeCacheMatrix defines a cached matrix class. The class contains 4 functions that allow an initial matrix to be stored, set, and called upon
+#further a inverse of a given matrix can be stored and called upon.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    setmatrix <-function(y){
+        x <<- y
+        inv <<- NULL}
+    getmatrix <- function() x
+    setinverse <- function(inverse) inv<<-inverse
+    getinverse <- function() inv
+    
+    list( getmatrix = getmatrix,
+          setmatrix = setmatrix,
+          setinverse = setinverse,
+          getinverse = getinverse)
+    
+    
 }
 
 
-## Write a short comment describing this function
+## CacheSolve function requires a matrix of the cached matrix class, the function looks to see if an inverse has been stored
+# if it has the inverse is called upon, if not the inverse is calculated then stored for future use
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x=matrix(), ...) {
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+        if(!is.null(inverse)==TRUE){
+            message("getting cached data")
+            return(inverse)
+            }
+        else{
+            message("no cached data, solving for matix")
+            inverse <- solve(x$getmatrix())
+            x$setinverse(inverse)
+            inverse
+            }
+    }
